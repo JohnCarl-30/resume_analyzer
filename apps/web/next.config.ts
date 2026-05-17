5361196234b2de118bf80589d8c998bf19514b2d
John Carl <johncarlsantos30@gmail.com>
2026-05-18 03:24:03 +0800

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
+import { join } from "node:path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: join(process.cwd(), "../.."),
+};
 
 export default nextConfig;
